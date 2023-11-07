2629488b18e2840b22aa7aaa5d4cad5c1e2dad67
Sarah Rehan <sarahahmad17@gmail.com>
2023-11-06 21:38:49 -0500

    Added Posts routes

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -31,3 +31,81 @@ func PostsCreate(c *gin.Context) {
 		"post": post,
 	})
 }
+
+func PostsIndex(c *gin.Context) {
+
+	// Get the posts
+	var posts []models.Post
+	result := initializers.DB.Find(&posts)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+	// Respond with them
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow(c *gin.Context) {
+
+	id := c.Param("id")
+
+	// Get post
+	var post models.Post
+	result := initializers.DB.First(&post, id)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Respond
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+
+	// Get id from URL
+	id := c.Param("id")
+
+	// Get data from req body
+	var body struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
+
+	c.Bind(&body)
+
+	// Find the post that needs to be updated
+	var post models.Post
+	result := initializers.DB.First(&post, id)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Update the post
+	initializers.DB.Model(&post).Updates(models.Post{Title: body.Title, Body: body.Body})
+
+	// Respond with the updated post
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsDelete(c *gin.Context) {
+
+	// Get id from URL
+	id := c.Param("id")
+
+	//	Delete the post
+	initializers.DB.Delete(&models.Post{}, id)
+
+	// Respond with the posts
+	c.Status(200)
+}
--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
